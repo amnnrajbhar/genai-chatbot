484b7d4bc782fba71db699c16345a60b9db805d0
amanrajbhar <amanrajbhar289@gmail.com>
2025-09-26 11:34:30 +0530

    Changed Flash Model 2.0

--- src/app/services/chatbot.service.ts
+++ src/app/services/chatbot.service.ts
@@ -10,7 +10,7 @@ export class ChatbotService {
 
   async generateResponse(message: string): Promise<string> {
     try {
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${this.apiKey}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${this.apiKey}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyDwzjDf4jHrcISoV3sHFeR2D9DWmt_lP90'
+  // apiKey: 'AIzaSyDwzjDf4jHrcISoV3sHFeR2D9DWmt_lP90'
+  apiKey: 'AIzaSyARY9c0mRdRCj3L5zAFkrwmKNg8HvFCkF8'
 };
